c6a1219e0e4098541bda6231d03c275446fbf046
AldyLoing <loingaldy@gmail.com>
2025-11-18 00:56:54 +0800

    Fix: Update urls to use 'posting.urls' instead of 'posting.urls_new'

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('posting.urls_new')),
+    path('', include('posting.urls')),
 ]
 
 # Serve static files in development
